47fdbdb0dbcd762f81dab30f63be33b4bfe86b50
0709anhnv <vananh257901@gmail.com>
2022-03-07 21:04:30 +0700

    demo

---
+++ dist/app.js
@@ -0,0 +1,12 @@
+"use strict";
+let a = 5;
+let b = 2.8;
+let phrase = 'Result is  ';
+let permit = true;
+const result = a + b;
+if (permit) {
+    console.log(phrase + result);
+}
+else {
+    console.log('Not show result');
+}
---
+++ src/app.ts
@@ -0,0 +1,15 @@
+type Person = {
+    name: string,
+    age: number,
+    id?: number,
+}
+
+const person: Person = {
+    name: "Van",
+    age: 1
+}
+
+const persons: Person[] = [
+    {id: 1, name: "A", age: 3},
+    {id: 2, name: "B", age: 4}
+]
