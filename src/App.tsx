c80c723ffbd140a8376885730e8b2d9374f1779b
0709anhnv <vananh257901@gmail.com>
2022-03-15 10:58:55 +0700

    feat: list && remove data && api

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import type { ProductType } from './types/product'
-import ShowInfo from './components/ShowInfo'
+import axios from 'axios'
+import { list, remove } from './api/product';
 
 function App() {
-  const [info, setInfo] = useState<ProductType>({
-    name: "Product A",
-    age: 20
-  })
+  const [product, setProduct] = useState<ProductType[]>([]);
+  useEffect(()=> {
+    const getProduct = async () => {
+      const { data } = await list()
+      setProduct(data)
+    }
+    getProduct();
+  }, [])
+
+  const removeItem = async (id: number) => {
+    const { data } = await remove(id);
+    data && setProduct(product.filter(item => item.id !== data.id))
+  }
 
   return (
-    <div>
-      <ShowInfo info={info}/>
+    <div className="App">
+      <table>
+        <thead>
+          <th>#</th>
+          <th>Name</th>
+          <th>Action</th>
+        </thead>
+        <tbody>
+          {product.map((item, index) => {
+            return <tr>
+                    <td>{index + 1}</td>
+                    <td>{item.name}</td>
+                    <td>
+                      <button onClick={() => removeItem(item.id)}>Remove</button>
+                    </td>
+                  </tr>
+          })}
+          
+        </tbody>
+      </table>
+      
     </div>
   )
 }
