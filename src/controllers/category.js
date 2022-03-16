e43c2e7c61eb2ae44750b1772c1f56c5dbeb3694
0709anhnv <vananh257901@gmail.com>
2022-03-16 12:17:39 +0700

    fix: read category slug

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -30,9 +30,10 @@ export const getOne = async (req, res) => {
   console.log(req.params.slug);
   try {
     const category = await Category.findOne({slug: req.params.slug}).exec();
-    console.log(category._id);
-    const products = await Products.find({ category: category._id }).populate('category').select('-category').exec();
-    res.json({category, products});
+    const products = await Products.find({ category: category }).populate('category').select('-category').exec();
+    res.json({
+      category, products
+    });
   } catch (error) {
     res.status(400).json({
       message: "Lỗi"
